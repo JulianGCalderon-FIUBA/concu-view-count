b5a77690e6c666df3de21cc81758548b641c4a51
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-09-04 15:17:02 -0300

    finish fastest

---
+++ src/semi-concurrent.rs
@@ -0,0 +1,32 @@
+use csv::Reader;
+use glob::glob;
+use rayon::prelude::*;
+use std::collections::HashMap;
+use std::fs::File;
+use view_count::utils::{add_key, merge_maps, time};
+use view_count::video::Video;
+
+fn main() {
+    let (time, ret) = time(run);
+    println!("Parallel: {:?}", time);
+    println!("Channels: {:?}", ret.keys().len());
+    println!("MasterChef 2017: {:?}", ret.get("MasterChef 2017"));
+}
+
+fn run() -> HashMap<String, usize> {
+    glob("data/*")
+        .unwrap()
+        .par_bridge()
+        .flatten()
+        .flat_map(Reader::from_path)
+        .map(|reader| parse_file(reader))
+        .reduce(|| HashMap::new(), merge_maps)
+}
+
+fn parse_file(reader: Reader<File>) -> HashMap<String, usize> {
+    reader
+        .into_records()
+        .flatten()
+        .map(Video::from)
+        .fold(HashMap::new(), add_key)
+}
